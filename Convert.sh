9960e4e20c8d4f23fc601599544214b9c913a271
Tanvir Ahmed Khan <takhandipu@gmail.com>
2016-05-29 20:51:10 +0600

    Template Created

---
+++ Convert.sh
@@ -0,0 +1,15 @@
+convert           \
+   -verbose       \
+   -density 150   \
+    $1.pdf      \
+   -quality 100   \
+   -sharpen 0x1.0 \
+    $1.png
+
+convert           \
+   -verbose       \
+   -density 150   \
+    $1.pdf      \
+   -quality 100   \
+   -sharpen 0x1.0 \
+    $1.jpg
